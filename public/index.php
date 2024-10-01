ce73ab0d9a5c6a69d7433d00a5dfb7bec46b632f
Luis-Alvarezz <luisangelug10@gmail.com>
2024-09-30 19:17:27 -0600

    File to control FrondEnd and BackEnd with vendor autoloadm using dotEnv and endpoints from routes.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,23 @@
-<!-- Archivo para backend -->
+<?php
+  // Archivo para backend y ser enlace con frontend
+
+  require __DIR__ . '/../vendor/autoload.php';
+
+  // use Dotenv\Dotenv;
+  use Symfony\Component\HttpFoundation\Request;
+  use Symfony\Component\HttpFoundation\Response;
+
+  // Lectra del archivo .env
+  $dotEnv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../') ;
+  $dotEnv->load();
+
+  // Lectura del archivo de rutas: 
+  $router = new \Bramus\Router\Router(); // Indicamos todos los metodos de endpoints
+
+  require __DIR__ . '/../app/routes/web.php';
+
+  $request = Request::createFromGlobals();
+  $response = new Response();
+
+  $router->run();
+  $response->send();
